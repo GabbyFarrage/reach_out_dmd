6fb8b80ae6edf6f083f2a57879fbcc557155ea8b
GabbyFarrage <ggfarrage@gmail.com>
2026-08-06 16:56:55 -0700

    Update out.js

--- out.js
+++ out.js
@@ -16,7 +16,7 @@ const arrowR = 40;
 async function setup() {
   createCanvas(windowWidth, windowHeight);
   hand = await loadImage('assets/hand.png');
-  hand.resize(500, 650);
+  hand.resize(350, 0);
   removeWhiteBackground(hand);
   textFont('Georgia');
   textSize(80);
